d33b96f72dc12a2a9459f8be3be2a06693226b1e
Leo Dion <leogdion@brightdigit.com>
2024-08-14 16:02:46 -0400

    refactoring for DataThespian

--- Package.swift
+++ Package.swift
@@ -5,17 +5,23 @@ import PackageDescription
 
 let package = Package(
     name: "DataThespian",
+    platforms: [.iOS(.v17), .macCatalyst(.v17), .macOS(.v14), .tvOS(.v17), .visionOS(.v1), .watchOS(.v10)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "DataThespian",
             targets: ["DataThespian"]),
     ],
+    dependencies: [
+      .package(url: "https://github.com/brightdigit/FelinePine.git", from: "1.0.0-beta.2")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "DataThespian"),
+            name: "DataThespian",
+            dependencies: ["FelinePine"]
+        ),
         .testTarget(
             name: "DataThespianTests",
             dependencies: ["DataThespian"]
---
+++ Sources/DataThespian/Logging.swift
@@ -0,0 +1,23 @@
+//
+//  Logging.swift
+//  DataThespian
+//
+//  Created by Leo Dion on 8/14/24.
+//
+
+import FelinePine
+
+public enum ThespianLogging : LoggingSystem {
+
+  
+  public enum Category : String, CaseIterable {
+    case application
+    case data
+  }
+  
+  
+}
+
+
+
+public protocol Loggable: FelinePine.Loggable where Self.LoggingSystemType == ThespianLogging {}

eac0b4a180658b96f787bb84dbba08a3e39d033d
Leo Dion <leogdion@brightdigit.com>
2024-10-29 13:41:17 -0400

    adding fetch multiple gets

--- Sources/DataThespian/Databases/Selector.swift
+++ Sources/DataThespian/Databases/Selector.swift
@@ -59,6 +59,14 @@
   }
 
   extension Selector.List {
+    public static func descriptor(_ type: T.Type, predicate: Predicate<T>? = nil, sortBy: [SortDescriptor<T>] = [], fetchLimit: Int? = nil) -> Selector.List {
+      .descriptor(.init(predicate: predicate, sortBy: sortBy, fetchLimit: fetchLimit))
+    }
+    
+    public static func descriptor(predicate: Predicate<T>? = nil, sortBy: [SortDescriptor<T>] = [], fetchLimit: Int? = nil) -> Selector.List {
+      .descriptor(.init(predicate: predicate, sortBy: sortBy, fetchLimit: fetchLimit))
+    }
+    
     public static func all(_ type: T.Type) -> Selector.List {
       .descriptor(.init())
     }

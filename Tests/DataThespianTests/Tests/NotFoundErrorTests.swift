5acdb5440e2f228ffb4a6abf40ceaedb07986233
Leo Dion <leogdion@brightdigit.com>
2025-06-02 16:27:12 -0400

    Fixing Tests

--- Tests/DataThespianTests/Tests/NotFoundErrorTests.swift
+++ Tests/DataThespianTests/Tests/NotFoundErrorTests.swift
@@ -16,7 +16,7 @@ import Testing
 
 @Suite(.enabled(if: swiftDataIsAvailable()))
 internal struct NotFoundErrorTests {
-  @Test internal func testAnyModelNotFoundError() throws {
+  @Test internal func testAnyModelNotFoundError() async throws {
     #if canImport(SwiftData)
       let database = try TestingDatabase(for: Parent.self, Child.self)
       try await database.withModelContext { context in
@@ -24,19 +24,19 @@ internal struct NotFoundErrorTests {
         let parent = Parent(id: UUID())
         context.insert(parent)
         try context.save()
-        
+
         // Get a valid PersistentIdentifier
         let persistentID = parent.persistentModelID
-        
+
         // Create a NotFoundError with the PersistentIdentifier
         let error = AnyModel.NotFoundError(persistentIdentifier: persistentID)
-        
+
         // Verify the error contains the correct PersistentIdentifier
         #expect(error.persistentIdentifier.id == persistentID.id)
-        
+
         // Create a NotFoundError with the same PersistentIdentifier for the typed Model
         let typedError = Model<Parent>.NotFoundError(persistentIdentifier: persistentID)
-        
+
         #expect(error.persistentIdentifier.id == typedError.persistentIdentifier.id)
       }
     #endif

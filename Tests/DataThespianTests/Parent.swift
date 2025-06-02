04ffe8aac42949688ecd995e485c81b5967d5ffe
Leo Dion <leogdion@brightdigit.com>
2025-06-02 08:15:09 -0400

    adding more tests

--- Tests/DataThespianTests/Parent.swift
+++ Tests/DataThespianTests/Parent.swift
@@ -13,7 +13,7 @@
   internal class Parent {
     internal var id: UUID
     @Relationship(inverse: \Child.parent)
-    internal var children: [Child] = []
+    internal var children: [Child]? = []
     internal init(id: UUID) {
       self.id = id
     }

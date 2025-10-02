c726333055b57850c47c5599f8334dbae2b4076c
codefactor-io <support@codefactor.io>
2025-10-02 18:24:02 +0000

    [CodeFactor] Apply fixes

--- Sources/DataThespian/Databases/ModelActor+Database.swift
+++ Sources/DataThespian/Databases/ModelActor+Database.swift
@@ -28,9 +28,9 @@
 //
 
 #if canImport(SwiftData)
+import Foundation
   import os.log
   public import SwiftData
-import Foundation
 
   extension ModelActor where Self: Database {
     /// A Boolean value indicating whether the current thread is the background thread.
@@ -67,11 +67,11 @@ import Foundation
 
       let persistentIdentifier = model.persistentIdentifier
       // return try closure(self[model.persistentIdentifier, as: PersistentModelType.self])
-      
+
       let fetchDescriptor = FetchDescriptor<PersistentModelType>(predicate: #Predicate{
         $0.persistentModelID == persistentIdentifier
-      } ,fetchLimit: 1)
-      
+      }, fetchLimit: 1)
+
       return try await self.withModelContext { modelContext in
         try closure(modelContext.fetch(fetchDescriptor).first)
       }
